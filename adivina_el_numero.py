8fc5e9532e884a2307cfae41a5137f972c1f639d
Marcos <pascualmarcos96@gmail.com>
2018-06-22 14:30:43 +0200

    Añadiendo el programa adivina_el_numero

---
+++ adivina_el_numero.py
@@ -0,0 +1,10 @@
+
+
+number_to_guess = 7
+
+user_number = int(input("Adivina un numero:"))
+
+if number_to_guess == user_number:
+    print("Felicidades, adivinaste el numero.")
+else:
+    print("Has Perdido.=(")
