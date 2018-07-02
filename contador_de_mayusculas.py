94783fa0867807763af5c21d0b057473f9e2bb62
Marcos <pascualmarcos96@gmail.com>
2018-07-02 17:38:14 +0200

    Contador de mayusculas en una string

--- contador_de_mayusculas.py
+++ contador_de_mayusculas.py
@@ -1,7 +1,13 @@
 
 texto_usuario = input("Introduce una frase: ")
 
+mayusculas = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+
 n_mayusculas = 0
 
 for letra in texto_usuario:
-    if letra in
+    if letra in mayusculas:
+        n_mayusculas += 1
+
+print("Hay un total de {} mayusculas en la frase {}".format(n_mayusculas,texto_usuario))
+
